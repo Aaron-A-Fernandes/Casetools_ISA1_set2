b4a6fda4e68e3542645b7d2fe6319e7d84d4fe62
exam <exam@Bcalab4-m01>
2024-02-07 09:18:55 +0530

    Commit

---
+++ src/Exam/Person.java
@@ -0,0 +1,13 @@
+package Exam;
+
+public class Person {
+	private String name;
+	private int age;
+	public Person(String name,int Age) {
+		this.name=name;
+		this.age=age;
+	}
+	public String getName() {
+		return name;
+	}
+}
